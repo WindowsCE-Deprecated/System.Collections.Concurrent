98acb9024bed8bc446f8dac1760a1ae475fdda75
Fabrício Godoy <fabricio@verion.ind.br>
2017-06-26 10:42:40 -0300

    Initial skeleton for project

---
+++ src/System.Collections.Concurrent.WindowsCE/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("System.Collections.Concurrent.WindowsCE")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("07c8524e-0f38-4298-aac6-806a4f7d6424")]
